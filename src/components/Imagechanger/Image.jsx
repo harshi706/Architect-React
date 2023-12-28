5e87e6a0d9098779121f6c01bf10e0d6b83559cd
Ehsan <ctech1699@gmail.com>
2023-12-28 23:35:00 +0530

    progress bar image changer

--- src/components/Imagechanger/Image.jsx
+++ src/components/Imagechanger/Image.jsx
@@ -1,22 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 
-const ProgressBar = ({ progress }) => (
-  <div className="absolute top-[0px] left-[0px] w-[100%] h-[5px] bg-white ">
-    <div
-      className=" bg-[#808080] h-full"
-      style={{
-        width: `${progress}%`,
-        transition: "width 3s",
-      }}
-    ></div>
-  </div>
-);
-
-const Image = () => {
+const Imagechanger = () => {
   const [index, setIndex] = useState(0);
-  const [carouselIndex, setCarouselIndex] = useState(0);
-  const [progress, setProgress] = useState(0);
+  const [openStates, setOpenStates] = useState([false, false, false]); // State to track open/close state
 
   const text = [
     "dsadasdorem ipsum dolor sit amet, consectetur adipissit amet, consect eiusmod tempor incididunt ut labore et dolore magnasit amet, consect eiusmod tempor incididunt ut labore et dolore magnacing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
@@ -24,157 +11,162 @@ const Image = () => {
     "lorem ipsum dolor sit amet, consectetur adipisci dolor sit amet, consectetur adipisci dolor sit amet, consectetur adipisci dolor sit amet, consectetur adipisci dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
   ];
 
-  const images = [
-    [
-      "https://images.unsplash.com/photo-1701293773241-de1a7bff8e3d?q=80&w=1141&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      "https://images.unsplash.com/photo-1701293773299-423854fabb0a?q=80&w=1138&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      "https://images.unsplash.com/photo-1701295463149-e8ab9a5acc71?q=80&w=1530&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    ],
-    [
-      "https://images.unsplash.com/photo-1701293773241-de1a7bff8e3d?q=80&w=1141&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      "https://images.unsplash.com/photo-1701293773299-423854fabb0a?q=80&w=1138&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      "https://images.unsplash.com/photo-1701295463149-e8ab9a5acc71?q=80&w=1530&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    ],
-    [
-      "https://images.unsplash.com/photo-1701293773241-de1a7bff8e3d?q=80&w=1141&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      "https://images.unsplash.com/photo-1701293773299-423854fabb0a?q=80&w=1138&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      "https://images.unsplash.com/photo-1701295463149-e8ab9a5acc71?q=80&w=1530&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    ],
+  const img = [
+    "https://images.unsplash.com/photo-1701295463149-e8ab9a5acc71?q=80&w=1530&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1701293773241-de1a7bff8e3d?q=80&w=1141&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1701293773299-423854fabb0a?q=80&w=1138&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+
+  ];
+  const img2 = [
+    "https://images.unsplash.com/photo-1701293773241-de1a7bff8e3d?q=80&w=1141&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1701295463149-e8ab9a5acc71?q=80&w=1530&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1701293773299-423854fabb0a?q=80&w=1138&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  ];
+  const img3 = [
+    "https://images.unsplash.com/photo-1701293773299-423854fabb0a?q=80&w=1138&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1701295463149-e8ab9a5acc71?q=80&w=1530&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1701293773241-de1a7bff8e3d?q=80&w=1141&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  ];
+  const [imageSrc, setImageSrc] = useState(img);
+  const Heading = [
+    "Watch your heart",
+    "Tune into your wellness",
+    "Improve your sleep habits",
   ];
 
-  const Heading = ["Trending Choices", "Trending Choices", "Trending Choices"];
-
-  const toggleIndex = (clickedIndex) => {
-    setIndex((prevIndex) =>
-      prevIndex === clickedIndex ? -1 : prevIndex === -1 ? clickedIndex : -1
+  const handleToggle = (clickedIndex) => {
+    const newOpenStates = openStates.map((state, idx) =>
+      idx === clickedIndex ? !state : false
     );
-    setCarouselIndex(0);
-  };
+    setOpenStates(newOpenStates);
+  
+    // Reset progress values when manually toggling
+    setProgress1(0);
+    setProgress2(0);
+    setProgress3(0);
+  
+    if (clickedIndex === 0) {
+      setimgIndex(0);
+      setImageSrc(img);
+    } else if (clickedIndex === 1) {
+      setimgIndex(0);
 
-  const handleCarouselClick = (increment) => {
-    const totalImages = images[index]?.length || 1; // Avoid division by zero if there are no images
-    setCarouselIndex(
-      (prevIndex) => (prevIndex + increment + totalImages) % totalImages
-    );
-  };
+      setImageSrc(img2);
+    } else if (clickedIndex === 2) {
+      setimgIndex(0);
 
-  const renderImages = () => {
-    if (index === -1) {
-      return <img src={images[0][0]} alt="" className={`w-[100%] h-[100%]`} />;
+      setImageSrc(img3);
     }
-
-    return images[index].map((src, imgIdx) => (
-      <img
-        key={imgIdx}
-        src={src}
-        alt=""
-        className={` w-[100%] h-[100%] ${
-          imgIdx === carouselIndex ? "" : "hidden"
-        }`}
-      />
-    ));
   };
-
+  //progress
+  const [progress1, setProgress1] = useState(0);
+  const [progress2, setProgress2] = useState(0);
+  const [progress3, setProgress3] = useState(0);
+  const [imgindex, setimgIndex] = useState(0);
   useEffect(() => {
-    const totalImages = images[index]?.length || 1; // Avoid division by zero if there are no images
-
     const interval = setInterval(() => {
-      if (index !== -1) {
-        setCarouselIndex((prevIndex) => (prevIndex + 1) % totalImages);
-        setProgress(
-          (prevProgress) => (prevProgress + (1 / totalImages) * 100) % 100
+      setProgress1((prevProgress) =>
+        prevProgress >= 100 ? 100 : prevProgress + 1
+      );
+
+      if (progress1 === 100) {
+        setimgIndex(1);
+        setProgress2((prevProgress) =>
+          prevProgress >= 100 ? 100 : prevProgress + 1
         );
       }
-    }, 3000);
+
+      if (progress2 === 100 && progress1 === 100) {
+        setimgIndex(2);
+        setProgress3((prevProgress) =>
+          prevProgress >= 100 ? 100 : prevProgress + 1
+        );
+      }
+    }, 30);
 
     return () => clearInterval(interval);
-  }, [index, images]);
+  }, [progress1, progress2, progress3]);
 
-  useEffect(() => {
-    setProgress(0);
-  }, [index]);
-
+  //
   return (
     <>
-      <div className="main-image w-full h-100  flex gap-[10px] sm:flex-row flex-col sm:px-[50px] px-[20px]">
-        <div className="left-image text-container flex flex-col m-auto justify-center sm:w-1/2 w-full h-full">
-          <div className="sm:text-3xl text-xl font-bold mb-2">
-            Trending adfsdd fjddf dffg <br />
-            fgdjd vfvdnd
+      <div className="main w-full h-full bg-gray-100 rounded-[50px] flex p-2">
+        <div className="left text-container flex flex-col items-center justify-center w-1/2 h-full  p-1">
+          <div className="text-container ">
+            <h1 className="font-bold text-3xl">
+              Get to know your <br />
+              health
+            </h1>
+            <h3 className="font-bold  text-gray-500 ">
+              Bands shown may be sold seperately and may not be <br />
+              available in all countries
+            </h3>
           </div>
-          <div className="text-md font-bold mb-2">
-            Trending choices description fddg fjd rfhnkf lorem{" "}
-          </div>
-
           {Heading.map((value, idx) => (
             <div key={idx} className="mb-7">
               {idx === 1 && (
-                <>
-                  {/* <div className="line-horizontal dark-black-line h-1 w-full bg-gray-300" /> */}
-                  <div
-                    onClick={() => toggleIndex(idx)}
-                    className="flex items-center justify-between sm:w-auto w-[80vw]"
-                  >
-                    <h1 className="text-2xl sm:my-4 my-1 font-bold text-left cursor-pointer">
-                      {value}
-                    </h1>
-                    {index === idx ? (
-                      <FaChevronUp className="text-2xl my-4 font-sans text-gray-500 text-left cursor-pointer" />
-                    ) : (
-                      <FaChevronDown className="text-2xl my-4 text-gray-500 text-left cursor-pointer" />
-                    )}
-                  </div>
-                </>
+                <div className="line-horizontal dark-black-line h-1 w-full   bg-gray-300 " />
               )}
-              {idx !== 1 && (
-                <div
-                  onClick={() => toggleIndex(idx)}
-                  className="flex items-center justify-between  sm:w-auto w-[80vw] "
+              <div
+                onClick={() => handleToggle(idx)}
+                className="flex  items-center justify-between"
+              >
+                <h1
+                  className={`text-2xl mb-2 font-bold text-left cursor-pointer ${
+                    openStates[idx] && "open"
+                  }`}
                 >
-                  <h1 className="text-2xl mb-2 font-bold text-left cursor-pointer">
-                    {value}
-                  </h1>
-                  {index === idx ? (
-                    <FaChevronUp className="text-2xl my-4 text-gray-500 text-left cursor-pointer" />
-                  ) : (
-                    <FaChevronDown className="text-2xl my-4 text-gray-500 text-left cursor-pointer" />
-                  )}
-                </div>
-              )}
-              <div className="sm:w-[23rem] w-[80vw]">
+                  {value}
+                </h1>
+                {openStates[idx] ? (
+                  <FaChevronUp className="text-2xl my-4 text-gray-500 text-left cursor-pointer" />
+                ) : (
+                  <FaChevronDown className="text-2xl my-4 text-gray-500 text-left cursor-pointer" />
+                )}
+              </div>
+              <div className={`w-[23rem] ${openStates[idx] ? "open" : ""}`}>
                 <p className={`${idx + 1}text`}>
-                  {index === idx ? text[idx] : ""}
+                  {openStates[idx] ? text[idx] : ""}
                 </p>
-                {/* {idx === 1 && (
+                {idx === 1 && (
                   <hr className="line-horizontal dark-black-line h-1 w-full bg-gray-300" />
-                )} */}
+                )}
               </div>
             </div>
           ))}
         </div>
-        <div className="right-image image-container flex sm:w-1/2 w-full h-full">
-          <div className="flex w-full h-full justify-center  relative">
-            {progress > 0 && <ProgressBar progress={progress} />}
-            {renderImages()}
-            <div
-              className="absolute top-0 h-full flex items-center cursor-pointer"
-              onClick={() => handleCarouselClick(-1)}
-            >
-              <div className="text-3xl font-bold text-white"></div>
+        <div className="right image-container flex w-1/2 h-full p-1">
+          <div className="flex w-full h-full justify-center items-center relative">
+            <div className=" absolute top-0 left-0 w-full h-5 z-20 flex justify-center gap-2 items-center">
+              <div className="progressContainer1 bg-white w-1/5 rounded-lg h-2">
+                <div
+                  className="progressLine rounded-lg bg-slate-600 h-full"
+                  style={{ width: `${progress1}%` }}
+                ></div>
+              </div>
+              <div className="progressContainer2 bg-white w-1/5 rounded-lg h-2">
+                <div
+                  className="progressLine rounded-lg bg-slate-600 h-full"
+                  style={{ width: `${progress2}%` }}
+                ></div>
+              </div>
+              <div className="progressContainer3 bg-white w-1/5 rounded-lg h-2">
+                <div
+                  className="progressLine rounded-lg bg-slate-600 h-full"
+                  style={{ width: `${progress3}%` }}
+                ></div>
+              </div>
             </div>
-            <div
-              className="absolute top-0 right-0 h-full flex items-center cursor-pointer"
-              onClick={() => handleCarouselClick(1)}
-            >
-              <div className="text-3xl font-bold text-white"></div>
-            </div>
+            <img
+              src={imageSrc[imgindex]}
+              alt=""
+              className=" w-[100%] h-[100%]"
+            />
           </div>
         </div>
       </div>
-      <br />
-      <br />
     </>
   );
 };
 
-export default Image;
+export default Imagechanger;
