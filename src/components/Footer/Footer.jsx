4b9d6868e92fbdf9282278818a5719d80793925f
isksariful <cessinideveloper@gmail.com>
2023-12-28 22:04:25 +0530

    footer ui

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './styles.css'
 import Joinayatrio from './Footer_child/Join_Ayatrio_Family'
 import Ayatrio_Family from './Footer_child/Ayatrio_Family'
 import Ayatrio_Services from './Footer_child/Ayatrio_Services'
@@ -29,26 +30,26 @@ const Footer = () => {
           </div>
         </div>
       </div> */}
-      <div className="sm:px-[50px] px-[20px] mt-20 pt-10 bg-{#f5f5f5}">
+      <div className="sm:px-[50px] px-[20px] mt-20 pt-10 footer-container">
         <div className="filter_container flex gap-32">
           <div className="grid sm:grid-cols-6 grid-cols-1">
             <div className="sm:col-span-2 row-span-1 col-span-1 mr-12 mb-5">
               <Joinayatrio />
             </div>
             <hr className=" border w-[90vw] sm:hidden block" />
-            <div className="col-span-1 my-7">
+            <div className="col-span-1">
               <Ayatrio_Family />
             </div>
             <hr className=" border w-[90vw] sm:hidden block" />
-            <div className="col-span-1 my-7">
+            <div className="col-span-1">
               <Ayatrio_Services />
             </div>
             <hr className=" border w-[90vw] sm:hidden block" />
-            <div className="col-span-1 my-7">
+            <div className="col-span-1">
               <Help_Supports />
             </div>
             <hr className=" border w-[90vw] sm:hidden block" />
-            <div className="col-span-1 my-7">
+            <div className="col-span-1">
               <About_Ayatrio />
             </div>
             <hr className=" border w-[90vw] sm:hidden block" />
--- src/components/Footer/Footer_child/Join_Ayatrio_Family.jsx
+++ src/components/Footer/Footer_child/Join_Ayatrio_Family.jsx
@@ -6,7 +6,7 @@ const Joinayatrio = () => {
         <div className='filter_item'>
             <div>
                 <h4 className='filter_heading text-black'>Join Ayatrio Family</h4>
-                <img className='w-[20%]' src={Image} alt='' />
+                <img className='w-[40%]' src={Image} alt='' my-5 />
             </div>
             <p className='text-2xl'>Enjoy member-only discounts & offers, early access to Ayatrio sale, delicious gift offers and much more. Join for free.​</p>
             <p className='gray-text text-xl cursor-pointer'>Join the club</p>
