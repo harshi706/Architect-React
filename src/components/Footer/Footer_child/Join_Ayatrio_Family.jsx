df0f74ca7e0b78f96d308d1e6aec732c548b8588
isksariful <cessinideveloper@gmail.com>
2023-12-29 04:26:41 +0530

    new footer ui

--- src/components/Footer/Footer_child/Join_Ayatrio_Family.jsx
+++ src/components/Footer/Footer_child/Join_Ayatrio_Family.jsx
@@ -4,12 +4,12 @@ import Image from '../../../assets/ayatriologo.png';
 const Joinayatrio = () => {
     return (
         <div className='filter_item'>
-            <div>
-                <h4 className='filter_heading text-black'>Join Ayatrio Family</h4>
+            <h4 className='filter_heading text-black'>Join Ayatrio Family</h4>
+            <div className='footer_tcon'>
                 <img className='w-[40%]' src={Image} alt='' my-5 />
             </div>
-            <p className='text-2xl'>Enjoy member-only discounts & offers, early access to Ayatrio sale, delicious gift offers and much more. Join for free.​</p>
-            <p className='gray-text text-xl cursor-pointer'>Join the club</p>
+            <div className='foter-info'><p>Enjoy member-only discounts & offers, early access to Ayatrio sale, delicious gift offers and much more. Join for free.​</p></div>
+            <p className='w-[50%] bg-black pt-3 pb-3 pl-4 pr-4 rounded-full flex justify-center items-center text-white cursor-pointer'>Join the club</p>
         </div>
     );
 };
