175c436af27ce68d00965f05ceae0fee3836a7ec
isksariful <cessinideveloper@gmail.com>
2023-12-30 03:08:16 +0530

    new ui for home

--- src/components/Cards/Profile.jsx
+++ src/components/Cards/Profile.jsx
@@ -11,9 +11,10 @@ const Profile = () => {
   return (
     <div>
       <div className=' '>
-        <div className='grid sm:grid-cols-5 grid-cols-1 gap-4 bg-zinc-100 sm:px-[50px] px-[20px]  '>
+        <div className='grid sm:grid-cols-5 grid-cols-1 gap-4 bg-zinc-100 pt-11 sm:px-[50px] px-[20px]  '>
           <div className='col-span-2 sm:mb-auto mb-0'>
-            <div className='col-span-1 sm:ml-12  sm:mt-24 mt-4'>
+            {/* sm:ml-12  sm:mt-24 */}
+            <div className='col-span-1  mt-4'>
               <div className='mb-2 '>Inspiration to installation</div>
               <div className='font-bold sm:text-3xl text-xl mb-4'>
                 <span className='whitespace-nowrap'>Meet our designers,</span>
@@ -23,10 +24,10 @@ const Profile = () => {
                 to true
               </div>
               <div className='sm:text-xl text-lg mb-8'>
-              <span className='whitespace-nowrap'>We bring your vision to life with</span>
-              <br/>
-              <span className='whitespace-nowrap'>free white glove services</span>
-              <br/>
+                <span className='whitespace-nowrap'>We bring your vision to life with</span>
+                <br />
+                <span className='whitespace-nowrap'>free white glove services</span>
+                <br />
               </div>
               <button className='bg-black pt-3 pb-3 pl-4 pr-4 mb-24 mt-12 rounded-full flex justify-center items-center text-white'>
                 Book an appointment
