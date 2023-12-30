a73b25fc468f0501b36986eabade93a2bbfcc820
isksariful <cessinideveloper@gmail.com>
2023-12-30 14:31:53 +0530

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -103,7 +103,7 @@ function Header({ howMuchScrolled }) {
     >
       {isLoading && <TopLoader />}
       {!searchQuery ? (
-        <div className="navbar sm:px-[50px] px-[20px] py-0 flex justify-between items-center w-full">
+        <div className="navbar  sm:px-[50px] px-[20px] py-0 flex justify-between items-center w-full">
           <div className="left flex items-center sm:gap-5 gap-1 ">
             <div className="profile-menu font-bold p-[7px] hover:bg-slate-200 hover:rounded-full">
               <Menu />
